9751a5aed0f544e72b33bc7c72c50bfbf609ebe9
rell-xs <farelfalensio14@gmail.com>
2026-02-17 20:00:03 +0700

    Update product details and images in script.js

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 // ===== DATA PRODUK =====
 const products=[
-  {game:'ml', name:'ML Sultan', desc:'Skin epic + mythic', price:'Rp300.000', badge:'badge_best.jpg', images:['ml1_1.jpg','ml1_2.jpg','ml1_3.jpg','ml1_4.jpg','ml1_5.jpg'], wa:'6285184105776'},
-  {game:'ml', name:'ML Epic', desc:'Skin legend', price:'Rp280.000', badge:'', images:['ml2_1.jpg','ml2_2.jpg','ml2_3.jpg','ml2_4.jpg','ml2_5.jpg'], wa:'6285184105776'},
-  {game:'ml', name:'ML Legend', desc:'Full skin', price:'Rp350.000', badge:'badge_best.jpg', images:['ml3_1.jpg','ml3_2.jpg','ml3_3.jpg','ml3_4.jpg','ml3_5.jpg'], wa:'6285184105776'},
+  {game:'ml', name:'ML Sultan', desc:'Skin epic + mythic', price:'Rp300.000', badge:'ml1.jpg', images:['ml2.jpg'], wa:'6285184105776'},
+  {game:'ml', name:'ML Epic', desc:'Skin legend', price:'Rp280.000', badge:'', images:['ml3.jpg'], wa:'6285184105776'},
+  {game:'ml', name:'ML Legend', desc:'Full skin', price:'Rp350.000', badge:'ml1.jpg.jpg', images:['ml3_1.jpg'], wa:'6285184105776'},
   {game:'ml', name:'ML Mythic', desc:'Skin terbatas', price:'Rp400.000', badge:'', images:['ml4_1.jpg','ml4_2.jpg','ml4_3.jpg','ml4_4.jpg','ml4_5.jpg'], wa:'6285184105776'},
   {game:'ml', name:'ML Starter', desc:'Skin dasar', price:'Rp150.000', badge:'', images:['ml5_1.jpg','ml5_2.jpg','ml5_3.jpg','ml5_4.jpg','ml5_5.jpg'], wa:'6285184105776'},
 
-  {game:'ff', name:'FF Rare', desc:'Bundle langka', price:'Rp150.000', badge:'', images:['ff1_1.jpg','ff1_2.jpg','ff1_3.jpg','ff1_4.jpg','ff1_5.jpg'], wa:'6285184105776'},
-  {game:'ff', name:'FF Epic', desc:'Bundle eksklusif', price:'Rp180.000', badge:'', images:['ff2_1.jpg','ff2_2.jpg','ff2_3.jpg','ff2_4.jpg','ff2_5.jpg'], wa:'6285184105776'},
-  {game:'ff', name:'FF Legend', desc:'Full bundle', price:'Rp200.000', badge:'badge_best.jpg', images:['ff3_1.jpg','ff3_2.jpg','ff3_3.jpg','ff3_4.jpg','ff3_5.jpg'], wa:'6285184105776'},
+  {game:'ff', name:'FF Rare', desc:'Bundle langka', price:'Rp150.000', badge:'', images:['ff1.jpg'], wa:'6285184105776'},
+  {game:'ff', name:'FF Epic', desc:'Bundle eksklusif', price:'Rp180.000', badge:'', images:['ff2.jpg'], wa:'6285184105776'},
+  {game:'ff', name:'FF Legend', desc:'Full bundle', price:'Rp200.000', badge:'ff3.jpg', images:['ff4.jpg'], wa:'6285184105776'},
   {game:'ff', name:'FF Starter', desc:'Bundle dasar', price:'Rp120.000', badge:'', images:['ff4_1.jpg','ff4_2.jpg','ff4_3.jpg','ff4_4.jpg','ff4_5.jpg'], wa:'6285184105776'},
   {game:'ff', name:'FF Pro', desc:'Bundle premium', price:'Rp250.000', badge:'', images:['ff5_1.jpg','ff5_2.jpg','ff5_3.jpg','ff5_4.jpg','ff5_5.jpg'], wa:'6285184105776'},
 
-  {game:'rb', name:'Roblox Premium', desc:'Item eksklusif', price:'Rp200.000', badge:'', images:['rb1_1.jpg','rb1_2.jpg','rb1_3.jpg','rb1_4.jpg','rb1_5.jpg'], wa:'6285184105776'},
-  {game:'rb', name:'Roblox Rare', desc:'Item limited', price:'Rp220.000', badge:'badge_best.jpg', images:['rb2_1.jpg','rb2_2.jpg','rb2_3.jpg','rb2_4.jpg','rb2_5.jpg'], wa:'6285184105776'},
+  {game:'rb', name:'Roblox Premium', desc:'Item eksklusif', price:'Rp200.000', badge:'', images:['rb1.jpg'], wa:'6285184105776'},
+  {game:'rb', name:'Roblox Rare', desc:'Item limited', price:'Rp220.000', badge:'rb2.jpg', images:['rb2.jpg'], wa:'6285184105776'},
   {game:'rb', name:'Roblox Epic', desc:'Item langka', price:'Rp250.000', badge:'', images:['rb3_1.jpg','rb3_2.jpg','rb3_3.jpg','rb3_4.jpg','rb3_5.jpg'], wa:'6285184105776'},
   {game:'rb', name:'Roblox Starter', desc:'Item dasar', price:'Rp180.000', badge:'', images:['rb4_1.jpg','rb4_2.jpg','rb4_3.jpg','rb4_4.jpg','rb4_5.jpg'], wa:'6285184105776'},
   {game:'rb', name:'Roblox Pro', desc:'Item premium', price:'Rp300.000', badge:'', images:['rb5_1.jpg','rb5_2.jpg','rb5_3.jpg','rb5_4.jpg','rb5_5.jpg'], wa:'6285184105776'},
